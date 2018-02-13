e5c6b067ab1cab2b8b2db29e329664577c48819f
swapnil-shirke <swapnil.shirke@raweng.com>
2018-02-13 16:50:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('bltc4f7ed1f03053fe8')
+var app = Built.App('bltdcfe6da36e210b5a')
 .setHost("test-stag-api.built.io")
 .setProtocol("https")
-.setMasterKey('blt1247e1bd3347ffd1')
+.setMasterKey('blt6b728340670c84ab')
 
 // 574df6c8fb2034b1603d792e8e2922268779c456 - access token
 // bltc88f3d42ad8a728c - secret key
@@ -12,8 +12,8 @@ var app = Built.App('bltc4f7ed1f03053fe8')
 // web hook url - https://test-stag-api.built.io/v1/extensions/blt_ext_default/redeploy?application_api_key=bltc4f7ed1f03053fe8
 
 var extensionSDK = app.Extension({
-	extension_key  : 'blt_ext_default_test_app',
-	secret_key     : 'app_test_123',
+	extension_key  : 'blt_ext_default',
+	secret_key     : 'swapnil',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
 })
