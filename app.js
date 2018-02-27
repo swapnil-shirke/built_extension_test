76248751f2a2c28ad552517ea62139b1699f27e7
swapnil-shirke <swapnil.shirke@raweng.com>
2018-02-27 14:56:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('blt8b104e2a85d96a9d')
+var app = Built.App('blt553c6fce2d42cf10')
 .setHost("stag-api.built.io")
 .setProtocol("https")
-.setMasterKey('blt4eacfe5b25245d5e')
+.setMasterKey('blt5c486e7539e4dacc')
 
 // 574df6c8fb2034b1603d792e8e2922268779c456 - access token
 // bltc88f3d42ad8a728c - secret key
