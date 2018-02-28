470b598e80618d3665c0ea1ecebef6723c3f9c87
swapnil-shirke <swapnil.shirke@raweng.com>
2018-02-28 16:54:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('bltb5d916c1a054bdd0')
+var app = Built.App('bltbd2542510c6b8afc')
 .setHost("api.built.io")
 .setProtocol("https")
-.setMasterKey('type/case no')
+.setMasterKey('blt677ddc84530e6685')
 
 // 574df6c8fb2034b1603d792e8e2922268779c456 - access token
 // bltc88f3d42ad8a728c - secret key
